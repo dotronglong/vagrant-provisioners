25d67b217819420eb50c695e5c14a7d081c8f1eb
Long Do <me@dotronglong.com>
2016-11-09 00:16:01 +0700

    Init master
    Add alpine linux provisions
    
    Add docker provision for alpine linux

---
+++ alpine/latest/docker.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+apk update
+
+apk add docker
+rc-update add docker boot
+service docker start
+addgroup docker
+adduser vagrant docker
+
+apk add py-pip
+pip install docker-compose
+
+apk add nfs-utils
+rc-update add nfs boot
+rc-update add netmount
+rc-service nfs start
+rc-service netmount start
