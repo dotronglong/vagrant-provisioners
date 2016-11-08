0d2260554060c1f6309b63c1bdcbf3329fe91565
Long Do <me@dotronglong.com>
2016-11-09 00:50:21 +0700

    Rearrange commands

--- alpine/latest/docker.sh
+++ alpine/latest/docker.sh
@@ -6,11 +6,11 @@ rc-update add docker boot
 service docker start
 adduser vagrant docker
 
-apk add py-pip
-pip install docker-compose
-
 apk add nfs-utils
 rc-update add nfs boot
 rc-update add netmount
 rc-service nfs start
 rc-service netmount start
+
+apk add py-pip
+pip install docker-compose
