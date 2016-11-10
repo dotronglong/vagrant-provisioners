3af18c7b13780b439e68f753c8b49c2d83bdc71c
Do Trong Long <me@dotronglong.com>
2016-11-10 13:41:10 +0700

    Update docker.sh
    Remove NFS

--- alpine/latest/docker.sh
+++ alpine/latest/docker.sh
@@ -6,11 +6,5 @@ rc-update add docker boot
 service docker start
 adduser vagrant docker
 
-apk add nfs-utils
-rc-update add nfs boot
-rc-update add netmount
-rc-service nfs start
-rc-service netmount start
-
 apk add py-pip
 pip install docker-compose
